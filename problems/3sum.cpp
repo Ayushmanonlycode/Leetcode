5bae3edbc0cd05cdd2d149b556784361172cf1e1
Ayushmanonlycode <ayushman.onlycode69@gmail.com>
2025-07-27 23:55:00 +0530

    lc 15

---
+++ problems/3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>result;
+        set<vector<int>>set;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+           
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k)
+            {
+                 int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    
+                    set.insert({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+                else if (sum<0) j++;
+                else k--;
+            }
+        }
+        for(auto n: set)
+        result.push_back(n);
+
+        return result;
+        
+    }
+};
